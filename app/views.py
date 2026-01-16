4f5a5b8fd9aa334ec7861b27c6afe8d8d194d158
Salman Meer <sulligabbar@gmail.com>
2026-01-16 11:41:32 +0500

    Initial commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated, )
+    def get(self, request):
+        user=request.user
+        content = {'message': 'Hello, GeeksforGeeks','username':user.username}
+        return Response(content)
